d633e208a140098e494122035839232932221c4b
thurman sanders <thurman.sanders@yahoo.com>
2025-12-17 22:26:37 -0500

    changed name to foo

--- src/main/java/com/amigoscode/product/ProductService.java
+++ src/main/java/com/amigoscode/product/ProductService.java
@@ -57,7 +57,7 @@ public class ProductService {
     Function<Product, ProductResponse> mapToResponse() {
         return p -> new ProductResponse(
                 p.getId(),
-                p.getName(),
+                "foo",
                 p.getDescription(),
                 p.getPrice(),
                 p.getImageUrl(),
