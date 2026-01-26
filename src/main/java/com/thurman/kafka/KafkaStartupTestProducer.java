327b9c8bde7372a3b08f8bd63cb2887184ce7e35
thurman sanders <thurman.sanders@yahoo.com>
2026-01-26 18:17:18 -0500

    fix kafka stuff

--- src/main/java/com/thurman/kafka/KafkaStartupTestProducer.java
+++ src/main/java/com/thurman/kafka/KafkaStartupTestProducer.java
@@ -2,10 +2,16 @@ package com.thurman.kafka;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
+@ConditionalOnProperty(
+        name = "app.kafka.startup-producer.enabled",
+        havingValue = "true",
+        matchIfMissing = false
+)
 public class KafkaStartupTestProducer implements CommandLineRunner {
 
     private final KafkaTemplate<String, String> kafkaTemplate;
